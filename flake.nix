23a0571f362a148b6b7bdc7cc41d3a1e70df1458
cole <coljnr9@hotmail.com>
2026-03-14 09:30:45 -0700

    Set up nix for this project

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "alspec dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python312
+            uv
+            ruff
+            ripgrep
+            fish
+          ];
+
+          shellHook = ''
+            echo "alspec dev shell ready"
+          '';
+        };
+      });
+}
